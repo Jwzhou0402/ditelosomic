a41fe33b16143b88ed479c29fbdffa236e53ac7e
Jwzhou0402 <1297447362@qq.com>
2024-09-11 16:45:36 +0800

    Create DNA_Motif_NonBDNA.sh

---
+++ DNA_Motif_NonBDNA.sh
@@ -0,0 +1,169 @@
+
+#####01_bwa index
+bsub -J fastp -n 8 -o 01_index-%J.out -e 01_index-%J.err -R span[hosts=1] "bwa index -a bwtsw wheat.CSGL.fa"
+
+#####02_qc
+fastqc -o /public/home/jingwzhou/Data/CHIP_seq/H3K4/QC_after/trimmomatic/ /public/home/jingwzhou/Data/CHIP_seq/H3K4/QC_after/trimmomatic/*paired.fq.gz;\
+
+for sample in ./*_1.fq.gz; do
+index=$(basename $sample |sed 's/_1.fq.gz//')
+prefix=$(dirname $sample)
+bsub -J fastp -n 8 -o 01_qc-%J.out -e 01_qc-%J.err -R span[hosts=1] "fastp -p -w 15 -l 30 -i ${prefix}/${index}_1.fq.gz -I ${prefix}/${index}_2.fq.gz -o ${prefix}/${index}_clean.R1.fastq.gz -O ${prefix}/${index}_clean.R2.fastq.gz -h ${prefix}/${index}.html"
+done
+
+#####03_mapping_dedupilcate_normalized
+for sample in ./*_1_clean.fq.gz; do
+
+index=$(basename $sample |sed 's/_1_clean.fq.gz//')
+prefix=$(dirname $sample)
+
+bsub -J ${index} -n 8 -o %J.${index}.out -e %J.${index}.err -R span[hosts=1] "bwa mem -t 10 ./bwa_index/wheat.CSGL.fa ${prefix}/${index}_1_clean.fq.gz ${prefix}/${index}_2_clean.fq.gz | awk '{if(\$0~/^@/||\$5>=20) {print \$0}}' | samtools sort -@ 20  -o ${index}_sort.bam;\
+
+java -jar /public/home/jingwzhou/miniconda3/share/picard-2.26.6-0/picard.jar MarkDuplicates REMOVE_DUPLICATES=true I=${prefix}/${index}_sort.bam O=${prefix}/${index}_sort_dedupilcate.bam METRICS_FILE=${prefix}/${index}_sort_metrics.txt ASSUME_SORTED=true VALIDATION_STRINGENCY=LENIENT TMP_DIR=tmp${prefix}/${index} ;\
+
+samtools index -c -@ 10 ${prefix}/${index}_sort_dedupilcate.bam;\
+
+samtools flagstat ${prefix}/${index}_sort_dedupilcate.bam > ${index}_sort_dedupilcate.flagstat;\
+
+bamToBed -i ${prefix}/${index}_sort_dedupilcate.bam | sort -k 1,1 -k 2,2n > ${prefix}/${index}_sort_dedupilcate.bed;\
+
+bamCoverage -of bigwig -b ${prefix}/${index}_dedupilcate.bam --binSize 50 --normalizeUsing RPGC --effectiveGenomeSize 14607800139 -p 10 -o ${index}_1Xnormalized_50.bw;\
+
+bamCoverage -b ${prefix}/${index}_dedupilcate.bam --binSize 10 --normalizeUsing RPKM -p 10 -o ${index}_RPKMnormalized_10.bw"
+sleep 10
+done
+
+
+#####04_CNV
+###04.1_cnvnator
+source /public/home/software/.bashrc
+module load CNVnator
+
+for sample in ./*_sort_dedupilcate.bam; do
+
+index=$(basename $sample |sed 's/_sort_dedupilcate.bam//')
+prefix=$(dirname $sample)
+
+bsub -J cnvnator -n 10 -o cnvnator-${index}_2000-%J.out -e cnvnator-${index}_2000-%J.err -R span[hosts=1] -q normal "cnvnator -genome /public/home/jingwzhou/Data/DNA_seq/12_CNV/split/wheat.CSGL.fa -root ${index}_2000.root -tree /public/home/jingwzhou/Data/DNA_seq/11_CALLSNP/06_rmdup/${index}_final.bam;\
+cnvnator -genome /public/home/jingwzhou/Data/DNA_seq/12_CNV/split/wheat.CSGL.fa -root ${index}_2000.root -his 2000 -d /public/home/jingwzhou/Data/DNA_seq/12_CNV/split;\
+cnvnator cnvnator -genome /public/home/jingwzhou/Data/DNA_seq/12_CNV/split/wheat.CSGL.fa -root ${index}_2000.root -stat 2000;\
+cnvnator -root ${index}_2000.root -eval 2000 > ${index}_2000.eval.ratio;\
+cnvnator -root ${index}_2000.root -partition 2000;\
+cnvnator -root ${index}_2000.root  -call 2000 > ${index}_2000.cnv;\
+cnvnator2VCF.pl ${index}_2000.cnv > ${index}_2000.cnv.vcf"
+done
+
+###04.2_mosdepth
+for sample in /public/home/jingwzhou/Data/DNA_seq/03_rmdu/*_dedupilcate.bam; do
+
+index=$(basename $sample |sed 's/_dedupilcate.bam//')
+prefix=$(dirname $sample)
+
+bsub  -J ${index} -n 8 -o %J.${index}.out -e %J.${index}.err -R span[hosts=1] "mosdepth -n -t 4 -F 1796 -T 1,2,3,4,5 --by 100000 ${prefix}/${index}_100Kb ${prefix}/${index}_dedupilcate.bam;\
+
+less ${index}_100Kb.regions.bed.gz | cut -f 1,2,4 | awk 'BEGIN{print "Chr\tStart\tCounts"}1' > ${index}_100Kb.regions_cov.txt"
+sleep 10
+done
+
+##04.3_plot
+source /public/home/software/.bashrc
+module load RStudio/4.2
+
+setwd("D:") 
+library(ggplot2)
+library(viridis)
+library(viridisLite)
+library(data.table)
+##############Dt1AL_100Kb_215-219.regions_cov.txt
+data1 <- read.delim("./Dt1AL_100Kb.regions_cov.txt") 
+data1_less2 <- subset(data1, Counts <=80) 
+pdf(paste0("Dt1AL_100Kb.pdf"), width=1.2, height=1)
+ggplot(data1_less2, aes(x = Start, y = Counts)) +
+  geom_area(fill = "#3C92C6", alpha = 1) +
+  scale_color_viridis("Depth", option = "viridis") +
+  theme_classic() +
+  theme(axis.line = element_line(size = 0.5))+
+  ylab("Read Depth") +
+  xlab("Position") +
+  ggtitle("Dt1AL") +
+  theme(plot.title = element_text(hjust = 0.5, size = 4, face = "bold"),
+        axis.title.x = element_text(size = 3, face = "bold"),
+        axis.title.y = element_text(size = 3, face = "bold"),
+        axis.text = element_text(size = 4, face = "bold"),
+        legend.key.height = unit(1,'cm')) +
+  scale_y_continuous(expand = expansion(mult = c(0.01, 0.1))) 
+dev.off()
+print
+
+########detection of chromosomal structural variations in wheat ditelosomic stocks
+cols<-c('#6EAC8B','#455E9B')
+pal<-colorRampPalette(cols)
+image(x=1:20,y=1,z=as.matrix(1:20),col=pal(20))
+Dt1BS_subset_less2 <- subset (Dt1BS_sort_100Kb.regions_cov, Counts <= 7) 
+pdf(paste0("Dt1BS_100Kb_240108.pdf"), width=3, height=1.5)
+ggplot(Dt1BS_subset_less2[CSDT_1BS_subset_less2$Chr=="chr7A",], aes(x = Start/1000000, y = Counts, color = Counts))+
+  geom_point(size = 0.05)+
+  scale_color_gradientn(colors = pal(20))+ 
+  theme_classic()+
+  ylab("Read Depth")+
+  xlab("Position")+
+  ggtitle("Dt1BS read depth of Chr7A within 100Kb window")+
+  theme(plot.title = element_text(hjust = 0.5,size = 4, face = "bold"),
+        axis.title.x = element_text(size = 5, face = "bold"),
+        axis.title.y = element_text(size = 5, face = "bold"),
+        axis.text = element_text(size = 5, face = "bold"),
+        legend.key.height = unit(0.5,'cm'),
+        legend.key.width = unit(0.2, 'cm') 
+  ) +
+  scale_x_continuous(labels = function(x) format(x, scientific = FALSE))
+dev.off()
+print(paste0())
+
+
+#####05_FindMotif
+source /public/home/software/.bashrc
+module load MEME/5.0.5
+
+bsub -J meme -n 8 -o motif_fimo%J.out -e motif_fimo%J.err -R span[hosts=1] -q smp "meme ../BreakPoint_1kb.fa  -dna -oc ./1000000_zoops_12_22 -nostatus -mod zoops -evt 0.05 -nmotifs 10 -minw 12 -maxw 22 -revcomp -maxsize 1000000"
+
+bsub -J fimo -n 8 -o motif_fimo%J.out -e motif_fimo%J.err -R span[hosts=1] -q smp "fimo -oc ./fimo/ ./meme.txt /public/home/jingwzhou/genome/CSGL/wheat.CSGL.fa"
+
+bsub -J 1Mb -n 3 -o %J.out -e %J.err -R span[hosts=1] "perl ./DNA_bed2-1Mb_wheat_CSGL.pl ./output_MEME-1.bed output_MEME-1_1Mb.txt;\
+perl ./DNA_bed2-1Mb_wheat_CSGL.pl ./output_MEME-2.bed output_MEME-2.Mb.txt;\
+perl ./DNA_bed2-1Mb_wheat_CSGL.pl ./output_MEME-3.bed output_MEME-3.Mb.txt"
+
+
+#####06_Non-B form DNA
+Whole genome Non-B form DNA sourced from Ma et al 2022
+
+####Centromere random
+samtools faidx wheat.CSGL.fa  > wheat.CSGL.fai
+bedtools makewindows -g wheat.CSGL.fai -w 10000 > 10Kb_windows.bed
+bedtools intersect -a ./CS_CEN.bed -b ./10Kb_windows.bed > CS_CEN_10kb.bed
+bedtools intersect -a ./CS_Arm.bed -b ./10Kb_windows.bed > CS_Arm_10kb.bed
+
+######Centromere random NONBDNA
+shuf -n 500 ./CS_CEN_10kb.bed | bedtools intersect -v -a - -b ./BreakPoint_10kb.bed | shuf -n 200 > CEN_random_200_10kb.bed;
+sort -k1,1 -k2,2n CEN_random_200_10kb.bed > sorted_CEN_random_200_10kb.bed;
+bedtools intersect -a ./sorted_CEN_random_200_10kb.bed -b ./wheat.CSGL.APR-DR-MR.bed  -wao > CENrandom_200_10kb_APR-DR-MR.txt
+
+#BreakPoint_10kb_APR-DR-MR
+bedtools intersect -a ./BreakPoint_10kb.bed -b ./wheat.CSGL.APR-DR-MR.bed  -wao > BreakPoint_10kb_APR-DR-MR.txt
+
+##########Chromosome arm random
+shuf -n 500 ./CS_Arm_10kb.bed | bedtools intersect -v -a - -b ./BreakPoint_10kb.bed | shuf -n 200 > Arm_random_200_10kb.bed
+sort -k1,1 -k2,2n Arm_random_200_10kb.bed > sorted_Arm_random_200_10kb.bed
+bedtools intersect -a ./sorted_Arm_random_200_10kb.bed -b ./wheat.CSGL.APR-DR-MR.bed  -wao > Arm_random_200_10kb_APR-DR-MR.txt
+
+######Non-B form DNA freeenergy
+bsub  -J samtools -n 1 -o getfasta.out -e getfasta.err -R span[hosts=1] -q q2680v2 "bedtools getfasta -fi /public/home/jingwzhou/genome/CSGL/wheat.CSGL.fa -bed ./APR-DR-MR_armrandom_200_1Kb.bed > ./APR-DR-MR_armrandom_200.fasta;\
+RNAfold --noGU --noconv --noPS -p -g < ./APR-DR-MR_armrandom_200.fasta > APR-DR-MR_armrandom_200.Energy.out;\
+rm *dp.ps"
+
+bsub  -J samtools -n 1 -o getfasta.out -e getfasta.err -R span[hosts=1] -q q2680v2 "bedtools getfasta -fi /public/home/jingwzhou/genome/CSGL/wheat.CSGL.fa -bed ./APR-DR-MR_Bp_1Kb.bed > ./APR-DR-MR_Bp.fasta;\
+RNAfold --noGU --noconv --noPS -p -g < ./APR-DR-MR_Bp_60.fasta > APR-DR-MR_Bp_60.Energy.out;\
+rm *dp.ps"
+
+bsub  -J samtools -n 1 -o getfasta.out -e getfasta.err -R span[hosts=1] -q q2680v2 "bedtools getfasta -fi /public/home/jingwzhou/genome/CSGL/wheat.CSGL.fa -bed ./APR-DR-MR_CENrandom_200_1Kb.bed > ./APR-DR-MR_CENrandom_200.fasta;\
+RNAfold --noGU --noconv --noPS -p -g < ./APR-DR-MR_CENrandom_200.fasta > APR-DR-MR_CENrandom_200.Energy.out;\
+rm *dp.ps"
